33e7543fef704e3b24983dcc5dbe8cf3538eecef
Devin Don <I.INF@Outlook.com>
2019-07-03 23:58:24 +0800

    refactor(client/app): use app service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { APPService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'client';
+  constructor(
+    public app: APPService
+  ) { }
 
 }
