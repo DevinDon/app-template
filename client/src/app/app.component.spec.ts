6eb66103ffd466b781df62cf23937654d97f1afb
Devin Don <I.INF@Outlook.com>
2019-07-04 00:29:32 +0800

    test(client/app): add unit test of app component

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,31 +1,45 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+import { APPService } from './app.service';
+
+// tslint:disable: component-selector
+@Component({ selector: 'router-outlet', template: '' })
+class RouterOutletComponent { }
+
+const appServiceStub: Partial<APPService> = {
+  title: 'Template',
+  desc: 'Web APP: Angular client + Rester server.'
+};
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        RouterOutletComponent
       ],
+      providers: [{ provide: APPService, useValue: appServiceStub }]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it('should be created', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('client');
-  });
-
-  it('should render title in a h1 tag', () => {
+  it('should render title in an h1.title tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to client!');
+    expect(compiled.querySelector('h1.title').textContent).toContain(appServiceStub.title);
+  });
+
+  it('should render desc in a p.desc tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p.desc').textContent).toContain(appServiceStub.desc);
   });
 });
