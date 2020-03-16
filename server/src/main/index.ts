2d6e34461911fd453c4a502fdcdfeded0e889d63
Devin Don <I.INF@Outlook.com>
2020-03-16 06:54:45 +0000

    feat(server): add version module

--- server/src/main/index.ts
+++ server/src/main/index.ts
@@ -1,9 +1,9 @@
 import { CORSHandler, Rester } from '@rester/core';
-import { MottoView } from './motto/motto.view';
+import { VersionView } from './version/version.view';
 
 const rester = new Rester()
   .configViews
-  .add(MottoView)
+  .add(VersionView)
   .end()
   .configHandlers
   .add(CORSHandler)
