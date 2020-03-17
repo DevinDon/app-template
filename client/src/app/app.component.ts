b7536f77748c7a2aa73131228970badd2d9c68d2
Devin Don <I.INF@Outlook.com>
2020-03-17 20:41:55 +0000

    feat(app): add toolbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,32 @@
 // src/app/app.component.ts
 
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AppService } from './service/app.service';
+import { destory } from './util/subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnDestroy {
+
+  title = 'Template';
+
+  private subscriptions: Subscription[] = [];
+
+  constructor(
+    public app: AppService
+  ) {
+    this.subscriptions.push(
+      app.observableRouterData()
+        .subscribe(data => this.title = data.title)
+    );
+  }
+
+  ngOnDestroy(): void {
+    destory(this.subscriptions);
+  }
+
+}
