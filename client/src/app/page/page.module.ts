25bed32343315ea5054e8ca4adaee9054005d126
Devin Don <I.INF@Outlook.com>
2020-07-20 17:05:14 +0800

    feat(page/drag): add drag page

--- client/src/app/page/page.module.ts
+++ client/src/app/page/page.module.ts
@@ -5,11 +5,12 @@ import { ComponentModule } from '../component/component.module';
 import { MaterialModule } from '../module/material.module';
 import { SharedModule } from '../module/shared.module';
 import { AboutComponent } from './about/about.component';
-import { UploadComponent } from './upload/upload.component';
+import { DragComponent } from './drag/drag.component';
 import { MoreComponent } from './more/more.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
-  declarations: [AboutComponent, UploadComponent, MoreComponent],
+  declarations: [AboutComponent, UploadComponent, MoreComponent, DragComponent],
   imports: [
     SharedModule,
     MaterialModule,
